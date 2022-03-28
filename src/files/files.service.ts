0c184487175a220c8eb9ba84d329ef74410fa95e
NikitaTkachev <nikitatkachevv@gmail.com>
2022-03-28 08:26:16 +0300

    feature(file-service): #10 check if the bucket exists and create it if not

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+// https://wanago.io/2020/08/03/api-nestjs-uploading-public-files-to-amazon-s3/
+// https://www.zenko.io/blog/first-things-first-getting-started-scality-s3-server/
+
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { Repository } from 'typeorm';
@@ -8,18 +11,38 @@ import { S3 } from 'aws-sdk';
 import PublicFileEntity from './public-file.entity';
 
 @Injectable()
-export class FilesService {
+export class FilesService implements OnModuleInit {
   constructor(
     @InjectRepository(PublicFileEntity)
     private readonly publicFileRepository: Repository<PublicFileEntity>,
     private readonly configService: ConfigService,
   ) {}
 
-  async uploadPublicFile(dataBuffer: Buffer, filename: string) {
-    const s3 = new S3({
+  onModuleInit(): any {
+    this.checkAndCreateBucket().then();
+  }
+
+  async checkAndCreateBucket(): Promise<void> {
+    const s3 = this.createS3Instance();
+    const bucket = this.configService.get('S3_PUBLIC_BUCKET_NAME');
+    try {
+      await s3.headBucket({ Bucket: bucket }).promise();
+    } catch (err) {
+      if (err.code === 'NotFound') {
+        await s3.createBucket({ Bucket: bucket }).promise();
+      }
+    }
+  }
+
+  private createS3Instance(): S3 {
+    return new S3({
       endpoint: this.configService.get('S3_ENDPOINT'),
       s3ForcePathStyle: true,
     });
+  }
+
+  async uploadPublicFile(dataBuffer: Buffer, filename: string) {
+    const s3 = this.createS3Instance();
     const extension = filename.split('.').reverse()[0];
     const uploadResult = await s3
       .upload({
@@ -40,10 +63,7 @@ export class FilesService {
 
   async deletePublicFile(fileId: number) {
     const file = await this.publicFileRepository.findOne({ id: fileId });
-    const s3 = new S3({
-      endpoint: this.configService.get('S3_ENDPOINT'),
-      s3ForcePathStyle: true,
-    });
+    const s3 = this.createS3Instance();
     await s3
       .deleteObject({
         Bucket: this.configService.get('S3_PUBLIC_BUCKET_NAME'),
