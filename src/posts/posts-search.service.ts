ae9dbeccc1b43d52fcf18b1356f4c38952ffb1ee
Nikita Tkachev <nikita.tkachev.dev@gmail.com>
2024-03-20 12:42:03 +0300

    refactor(search): Rework post-search according to the new ES API

--- src/posts/posts-search.service.ts
+++ src/posts/posts-search.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 import PostEntity from './post.entity';
-import { PostSearchResult } from './types/post-search-result.interface';
 import { PostSearchBody } from './types/post-search-body.interface';
 
 @Injectable()
@@ -14,9 +13,9 @@ export default class PostsSearchService {
   constructor(private readonly elasticSearchService: ElasticsearchService) {}
 
   async indexPost(post: PostEntity) {
-    return this.elasticSearchService.index<PostSearchResult, PostSearchBody>({
+    return this.elasticSearchService.index<PostSearchBody>({
       index: this.index,
-      body: {
+      document: {
         id: post.id,
         title: post.title,
         content: post.content,
@@ -25,60 +24,48 @@ export default class PostsSearchService {
     });
   }
 
-  async search(text: string) {
-    const { body } = await this.elasticSearchService.search<PostSearchResult>({
+  async search(text: string): Promise<PostSearchBody[]> {
+    const { hits } = await this.elasticSearchService.search<PostSearchBody>({
       index: this.index,
-      body: {
-        query: {
-          multi_match: {
-            query: text,
-            fields: ['title', 'content'],
-          },
+      query: {
+        multi_match: {
+          query: text,
+          fields: ['title', 'content'],
         },
       },
     });
-    const hits = body.hits.hits;
 
-    return hits.map((item) => item._source);
+    return hits.hits.map((item) => item._source);
   }
 
   async update(post: PostEntity) {
-    const newBody: PostSearchBody = {
-      id: post.id,
-      title: post.title,
-      content: post.content,
-      authorId: post.author.id,
-    };
-    // We need to write a script that updates all the necessary fields
-    // For example, to update the title and the content, we need:
-    // ctx._source.title='New title'; ctx._source.content= 'New content';
-    const script = Object.entries(newBody).reduce((result, [key, value]) => {
-      return `${result} ctx._source.${key}='${value}';`;
+    const script = Object.entries(post).reduce((result, [key, value]) => {
+      if (key !== 'id') {
+        return `${result} ctx._source.${key}='${value}';`;
+      }
+
+      return result;
     }, '');
 
     return this.elasticSearchService.updateByQuery({
       index: this.index,
-      body: {
-        query: {
-          match: {
-            id: post.id,
-          },
+      query: {
+        match: {
+          id: post.id,
         },
-        script: {
-          inline: script,
-        },
+      },
+      script: {
+        source: script,
       },
     });
   }
 
-  async remove(postId: number) {
+  async remove(postId: number): Promise<void> {
     await this.elasticSearchService.deleteByQuery({
       index: this.index,
-      body: {
-        query: {
-          match: {
-            id: postId,
-          },
+      query: {
+        match: {
+          id: postId,
         },
       },
     });
